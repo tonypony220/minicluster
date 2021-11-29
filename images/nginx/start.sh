3d7cbccec73b327f85c11851bed95c1e58b61827
tonypony220 <61715244+tonypony220@users.noreply.github.com>
2021-11-29 09:33:01 +0300

    auto

--- images/nginx/start.sh
+++ images/nginx/start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+cd /etc/nginx/
+openssl req -x509 -newkey rsa:4096 -nodes -keyout localhost.key -out localhost.crt -days 365 -subj '/CN=localhost'
+
 telegraf&
 nginx # -g 'daemon off;'
 wait -n 
